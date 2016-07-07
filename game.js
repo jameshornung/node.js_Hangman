9361b67af1e6200d26e5be40d61b3784f878bbac
James Hornung <hornung.james@gmail.com>
2016-07-06 21:26:40 -0500

    fully functional

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
 //A random word will be selected from this location
 
-exports.possibleWords = ['austin', 'phoenix', 'little rock', 'sacramento', 'hartford', 'denver', 'dover', 'tallahassee', 'des moines', 'baton rouge'];
+exports.possibleWords = ['austin', 'phoenix', 'little rock', 'sacramento', 'hartford', 'denver', 'dover', 'tallahassee', 'des moines', 'baton rouge', 'montgomery', 'juneau', 'honolulu', 'indianapolis', 'springfield', 'topeka', 'frankfort', 'lansing', 'saint paul', 'jefferson city', 'trenton', 'albany', 'oklahoma city', 'providence', 'pierre', 'salt lake city', 'montpelier', 'cheyenne'];
+
